5c4df857dc1e0488b90b28841905f583d063afaa
Lindsey Maratta <lindseymaratta@LindseyMaratta-MBP.local>
2017-12-26 16:26:30 -0500

    add js file and sticky sidebar

---
+++ base.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+// 	// $('body').click('body', function(){
+// 	// 	$(this).toggleClass('blue');
+// 	// });
+// 	$('body').scroll(function(){
+//     	if($('.body').scrollTop() > 20) {
+//         	$('.sidebar').addClass('sticky');
+//         	}
+//         	else {
+//         	$('.sidebar').removeClass('sticky');
+//     		};
+
+// 	});
+// });
+
+
